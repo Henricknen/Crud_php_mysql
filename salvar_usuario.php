b134bd9a695a94da6039258ed4dba8ed91e0620b
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-07-07 12:47:28 -0300

    utilizando_funcao_'md5'_para_criptografar_a_senha_e_criando_'alert'_de_usuario_nao_cadastrado_ou_usuario_cadastrado_com_sucesso

--- salvar_usuario.php
+++ salvar_usuario.php
@@ -3,12 +3,20 @@
         case 'cadastrar':
             $name = $_POST["nome"];
             $email = $_POST["email"];
-            $senha = $_POST["senha"];
+            $senha = md5($_POST["senha"]);      // 'md5' criptografa a senha
             $data_nasc = $_POST["data_nasc"];
 
             $sql = "INSERT INTO usuarios (nome, email, senha, data_nasc) VALUES ('{$name}', '{$email}', '{$senha}', '{$data_nasc}')";
 
             $res = $conn-> query($sql);
+
+            if($res == true) {
+                print "<script>alert('Cadastrado com sucesso');</script>";
+                print "<script>location.href = '?page = listar';</script>";
+            } else {
+                print "<script>alert('Não foi possível cadastrar');</script>";
+                print "<script>location.href = '?page = listar';</script>";                
+            }
             break;
 
         case 'editar':
