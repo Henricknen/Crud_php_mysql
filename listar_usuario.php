442ed6693a67c6f4133a7b70420a3793d9b3f9c7
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-07-08 19:38:35 -0300

    criando_arquivo_de_'listar_usuarios'_que_pucha_os_usuarios_gravados_no_banco_de_dados_e_apresenta_na_tela_o_'id'_'nome'_'email'_e_'data_de_nascimento'_dos_mesmos

--- listar_usuario.php
+++ listar_usuario.php
@@ -1,1 +1,29 @@
 <h1>Listar Usuários</h1>
+<?php 
+    $sql = "SELECT * FROM usuarios";        // 'consulta', seleçionando tudo da tabela usuarios
+
+    $res = $conn-> query($sql);     // conexão executando a 'query'
+
+    $qtd = $res-> num_rows;     // pegando o numero de linhas da consulta e armazenando na variável '$qtd'
+
+    if($qtd > 0) {
+        print "<table class = 'table table-hover table-striped table-bordered'>";
+        print "<tr>";
+        print "<th>#</th>";
+        print "<th>Nome</th>";
+        print "<th>E-mail</th>";
+        print "<th>Data de nascimento</th>";
+        print "</tr>";
+        while($row = $res-> fetch_object()) {       // 'fetch_object' traz os objetos do resultado da consulta e armazena na variável '$row'
+            print "<tr>";
+            print "<td>". $row-> id. "</td  >";        // imprimindo o objeto 'id'
+            print "<td>". $row-> nome. "</td>";
+            print "<td>". $row-> email. "</td>";
+            $data_nascimento = date('d/m/Y', strtotime($row->data_nasc));
+            print "<td>". $data_nascimento . "</td>";
+            print "</tr>";
+        }
+        print "</table>";
+    } else {
+        print "<p class = 'alert alert-danger'>Não encontrou resultado!</p>";
+    }
